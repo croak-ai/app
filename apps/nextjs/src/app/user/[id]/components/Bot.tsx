387ee745f8c1ecaddc8cc9ea4ec9d0c5f48cf063
Walshington <nicholaswalsh77@gmail.com>
2023-12-07 15:05:41 -0500

    testing

--- apps/nextjs/src/app/user/[id]/components/Bot.tsx
+++ apps/nextjs/src/app/user/[id]/components/Bot.tsx
@@ -3,6 +3,6 @@
 import { trpc } from "@next/app/_trpc/client";
 
 export default function Bot() {
-  const botRes = trpc.bot.createAssistant.useQuery();
+  const botRes = trpc.bot.createAssistant.useQuery("yo");
   return <>{JSON.stringify(botRes)}</>;
 }
--- packages/api/src/router/bot.ts
+++ packages/api/src/router/bot.ts
@@ -2,16 +2,16 @@ import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 import OpenAI from "openai";
 const openai = new OpenAI();
-const key = process.env;
+const key = process.env.OPENAI_API_KEY;
 
 export const botRouter = router({
-  createAssistant: publicProcedure
+  creatAssistant: publicProcedure
     .input(z.string().nullish())
     .query(async ({ ctx }) => {
       const completion = await openai.chat.completions.create({
         messages: [{ role: "system", content: "You are a helpful assistant." }],
         model: "gpt-3.5-turbo",
       });
-      return completion.choices[0]?.message.content;
+      return "not defined";
     }),
 });
