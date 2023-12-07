c26677d79b64f1e92ab2171825736d73c8e4c2c1
Walshington <nicholaswalsh77@gmail.com>
2023-12-07 13:48:17 -0500

    Openai added as nextjs dependency

--- apps/nextjs/src/app/user/[id]/components/Bot.tsx
+++ apps/nextjs/src/app/user/[id]/components/Bot.tsx
@@ -3,6 +3,6 @@
 import { trpc } from "@next/app/_trpc/client";
 
 export default function Bot() {
-  const botRes = trpc.bot.chat.useQuery();
+  const botRes = trpc.bot.createAssistant.useQuery();
   return <>{botRes.data}</>;
 }
--- packages/api/src/router/bot.ts
+++ packages/api/src/router/bot.ts
@@ -2,7 +2,9 @@ import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 
 export const botRouter = router({
-  chat: publicProcedure.input(z.string().nullish()).query(({ ctx }) => {
-    return "bot chat route hit";
-  }),
+  createAssistant: publicProcedure
+    .input(z.string().nullish())
+    .query(({ ctx }) => {
+      return "OpenAI assistant created";
+    }),
 });
