3c115ded3360bd2aa193c0fcd93a40512fbc874c
Walshington <nicholaswalsh77@gmail.com>
2023-12-07 16:01:00 -0500

    TRPC type issue solved

--- apps/nextjs/src/app/user/[id]/components/Bot.tsx
+++ apps/nextjs/src/app/user/[id]/components/Bot.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { trpc } from "@next/app/_trpc/client";
+// import { appRouter } from "@packages/api";
+// console.log("router: ", typeof appRouter);
 
 export default function Bot() {
-  const botRes = trpc.bot.createAssistant.useQuery("yo");
-  return <>{JSON.stringify(botRes)}</>;
+  const botRes = trpc.bot.createAssistant.useQuery();
+  return <>{JSON.stringify(botRes.data)} ayy</>;
 }
--- packages/api/src/router/bot.ts
+++ packages/api/src/router/bot.ts
@@ -1,17 +1,18 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 import OpenAI from "openai";
-const openai = new OpenAI();
-const key = process.env.OPENAI_API_KEY;
+//const openai = new OpenAI();
+//const key = process.env.OPENAI_API_KEY;
 
 export const botRouter = router({
-  creatAssistant: publicProcedure
+  createAssistant: publicProcedure
     .input(z.string().nullish())
     .query(async ({ ctx }) => {
-      const completion = await openai.chat.completions.create({
-        messages: [{ role: "system", content: "You are a helpful assistant." }],
-        model: "gpt-3.5-turbo",
-      });
+      console.log("WHATTTTT");
+      // const completion = await openai.chat.completions.create({
+      //   messages: [{ role: "system", content: "You are a helpful assistant." }],
+      //   model: "gpt-3.5-turbo",
+      // });
       return "not defined";
     }),
 });
