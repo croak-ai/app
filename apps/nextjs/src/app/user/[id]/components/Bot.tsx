3af9010650a69194ef2db3379c61c1443e8b2853
Walshington <nicholaswalsh77@gmail.com>
2023-12-07 17:16:18 -0500

    Final testing

--- apps/nextjs/src/app/user/[id]/components/Bot.tsx
+++ apps/nextjs/src/app/user/[id]/components/Bot.tsx
@@ -5,6 +5,6 @@ import { trpc } from "@next/app/_trpc/client";
 // console.log("router: ", typeof appRouter);
 
 export default function Bot() {
-  const botRes = trpc.bot.createAssistant.useQuery();
-  return <>{JSON.stringify(botRes.data)} ayy</>;
+  // const botRes = trpc.bot.createAssistant.useQuery();
+  return <>Hello</>;
 }
--- packages/api/src/router/bot.ts
+++ packages/api/src/router/bot.ts
@@ -1,18 +1,21 @@
 import { router, publicProcedure, protectedProcedure } from "../trpc";
 import { z } from "zod";
 import OpenAI from "openai";
-//const openai = new OpenAI();
+const openai = new OpenAI();
 //const key = process.env.OPENAI_API_KEY;
 
 export const botRouter = router({
   createAssistant: publicProcedure
     .input(z.string().nullish())
     .query(async ({ ctx }) => {
-      console.log("WHATTTTT");
-      // const completion = await openai.chat.completions.create({
-      //   messages: [{ role: "system", content: "You are a helpful assistant." }],
-      //   model: "gpt-3.5-turbo",
+      // const assistant = await openai.beta.assistants.create({
+      //   name: "Math Tutor",
+      //   instructions:
+      //     "You are a personal math tutor. Write and run code to answer math questions.",
+      //   tools: [{ type: "code_interpreter" }],
+      //   model: "gpt-4-1106-preview",
       // });
-      return "not defined";
+      // return assistant;
+      return "Hello";
     }),
 });
