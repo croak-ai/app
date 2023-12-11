494ab977e20e6688cda83fba33df86afb02497d6
Benjamin Werner <bewerner23@gmail.com>
2023-12-11 07:44:03 -0500

    adding shadcn to it's own package

--- apps/nextjs/src/app/layout.tsx
+++ apps/nextjs/src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@acme/ui/styles/globals.css";
 import Providers from "./providers";
 import { Suspense } from "react";
 
