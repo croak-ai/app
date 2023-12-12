461136227f68788f370221841d19bf0f5b2bd620
Walshington <nicholaswalsh77@gmail.com>
2023-12-12 12:52:23 -0500

    Prettier fixed, Tailwind config import broken

--- packages/api/express/src/routes/bot.ts
+++ packages/api/express/src/routes/bot.ts
@@ -1,6 +1,7 @@
-import app from '../../index'
+import app from "../../index";
+
+console.log("helloooo ");
 
 // app.get('/', (req: Request, res: Response) => {
 //     res.send('Hello, TypeScript Express!');
 //   });
-  
