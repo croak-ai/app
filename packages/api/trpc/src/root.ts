45c13522fa71598d8364e57e17c402392aabd724
Walshington <nicholaswalsh77@gmail.com>
2023-12-12 12:11:14 -0500

    TRPX fix, moving files around

--- packages/api/trpc/src/root.ts
+++ packages/api/trpc/src/root.ts
@@ -2,13 +2,11 @@ import { router } from "./trpc";
 import { authRouter } from "./router/auth";
 import { postRouter } from "./router/post";
 import { createNewTursoDB } from "./router/turso-meta/create-new-turso-db";
-import { botRouter } from "./router/bot";
 
 export const appRouter = router({
   post: postRouter,
   auth: authRouter,
   createNewTursoDB: createNewTursoDB,
-  bot: botRouter,
 });
 
 // export type definition of API
