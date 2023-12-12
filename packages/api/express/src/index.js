034333611bf2b8bc4c71b9ff4ca618a13041fa2c
Walshington <nicholaswalsh77@gmail.com>
2023-12-12 18:07:28 -0500

    Fastify configuration

--- packages/api/express/src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var express_1 = require("express");
-var app = (0, express_1.default)();
-var port = process.env.PORT || 3000;
-app.listen(port, function () {
-    console.log("Server running at http://localhost:".concat(port));
-});
-exports.default = app;
