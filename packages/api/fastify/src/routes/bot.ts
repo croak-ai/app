da71ebf6c2a41edd3c12f2e650a9947bb34ccc8c
Walshington <nicholaswalsh77@gmail.com>
2023-12-12 19:49:10 -0500

    typescript updated

--- packages/api/fastify/src/routes/bot.ts
+++
@@ -1,6 +0,0 @@
-import app from "../index";
-import { Request, Response } from "express";
-
-app.get("/express/test", (req: Request, res: Response) => {
-  res.send("Hello, TypeScript Express!");
-});
