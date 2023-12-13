372961d7e78695b69e726f17105c066d33a9257f
Benjamin Werner <bewerner23@gmail.com>
2023-12-13 01:04:09 -0500

    making db automatic

--- apps/nextjs/src/app/organization/[organizationSlug]/page.tsx
+++ apps/nextjs/src/app/organization/[organizationSlug]/page.tsx
@@ -1,24 +1,3 @@
-import { Button } from "@acme/ui/components/ui/button";
-import Link from "next/link";
-
-export default async function Page({
-  params,
-}: {
-  params: { organizationSlug: string };
-}) {
-  return (
-    <>
-      This is the organization page for {params.organizationSlug}
-      Click{" "}
-      <Link
-        href={`/organization/${params.organizationSlug}/create-db`}
-        className="text-primary underline"
-      >
-        this link
-      </Link>{" "}
-      <Button>asdasd</Button>
-      to create the database if you haven't yet. This will be better implemented
-      but it is 7am ok.{" "}
-    </>
-  );
+export default async function Page() {
+  return <>Hello</>;
 }
