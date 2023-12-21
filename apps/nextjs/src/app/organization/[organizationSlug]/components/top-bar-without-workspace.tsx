a65f146abf1ab696c26f7d3156b71d93a1b21478
Benjamin Werner <bewerner23@gmail.com>
2023-12-21 02:16:04 -0500

    Fixed weird flickering clerk issue

--- apps/nextjs/src/app/organization/[organizationSlug]/components/top-bar-without-workspace.tsx
+++ apps/nextjs/src/app/organization/[organizationSlug]/components/top-bar-without-workspace.tsx
@@ -4,14 +4,14 @@ export const TopBar = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="flex h-screen flex-col justify-center">
       <div className="grid w-full grid-cols-2 items-center py-3">
-        <div className="flex px-6">
+        <div className="flex h-[40px] px-6">
           <OrganizationSwitcher
             afterCreateOrganizationUrl="/organization/:slug"
             afterSelectPersonalUrl="/user/:id"
             afterSelectOrganizationUrl="/organization/:slug"
           />
         </div>
-        <div className="mr-12 justify-self-end">
+        <div className="mr-12  h-[40px] justify-self-end">
           <UserButton afterSignOutUrl="/" afterSwitchSessionUrl="/" />
         </div>
       </div>
