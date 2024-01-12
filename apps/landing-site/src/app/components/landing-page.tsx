d421ac28918b647312db74ba542bc8d1ff57f279
ben <bewerner23@gmail.com>
2024-01-12 18:09:37 -0500

    fixing window not defined

--- apps/landing-site/src/app/components/landing-page.tsx
+++ apps/landing-site/src/app/components/landing-page.tsx
@@ -1,7 +1,5 @@
 "use client";
 import Link from "next/link";
-import { Input } from "@acme/ui/components/ui/input";
-import { Button } from "@acme/ui/components/ui/button";
 import { CardContent, Card } from "@acme/ui/components/ui/card";
 import { Title } from "./title";
 import dynamic from "next/dynamic";
@@ -9,15 +7,15 @@ import dynamic from "next/dynamic";
 export default function Component() {
   const Meteors = dynamic(
     () => import("@acme/ui/components/aceternity/meteors"),
+    { ssr: false }, // This line will disable server-side rendering for the component
   );
 
   return (
     <div className="relative overflow-x-hidden">
       <div className="flex min-h-screen flex-col">
-        {" "}
         <Meteors number={20} />
         <main className="">
-          <section className="w-full py-2 sm:py-12 md:py-24 lg:py-32 xl:py-48">
+          <section className="w-full py-2 sm:py-24 ">
             <Title />
             <div className="container px-4 md:px-6">
               <div className="grid gap-6 lg:grid-cols-[1fr_400px] lg:gap-12 xl:grid-cols-[1fr_600px]">
