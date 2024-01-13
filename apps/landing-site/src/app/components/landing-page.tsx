edae76a7ce6eafd4ae154bde9eb147bd6ceea158
ben <bewerner23@gmail.com>
2024-01-12 21:22:29 -0500

    Adding Lamp Stuff

--- apps/landing-site/src/app/components/landing-page.tsx
+++ apps/landing-site/src/app/components/landing-page.tsx
@@ -1,9 +1,11 @@
 "use client";
 import Link from "next/link";
-import { CardContent, Card } from "@acme/ui/components/ui/card";
 import { Title } from "./title";
 import dynamic from "next/dynamic";
 import { ThreeDCardDemo } from "./main-site-image-demo";
+import DetailedCroakInfo from "./detailed-info";
+import { Icons } from "@acme/ui/components/bonus/icons";
+import { LampDemo } from "./lamp-section";
 
 export default function Component() {
   const Meteors = dynamic(
@@ -23,20 +25,8 @@ export default function Component() {
           <section className="w-full py-12 md:py-24 lg:py-32">
             <ThreeDCardDemo />
           </section>
-          <section className="w-full py-12 md:py-24 lg:py-32">
-            <div className="container grid items-center justify-center gap-4 px-4 text-center md:px-6">
-              <div className="space-y-3">
-                <h2 className="text-3xl font-bold tracking-tighter md:text-4xl/tight">
-                  About Croak AI
-                </h2>
-                <p className="mx-auto max-w-[600px] text-gray-500 dark:text-gray-400 md:text-xl/relaxed lg:text-base/relaxed xl:text-xl/relaxed">
-                  Croak AI is a startup on a mission to revolutionize
-                  communication with AI and LLMs. We believe in the power of
-                  effective communication and are committed to providing
-                  innovative solutions for businesses.
-                </p>
-              </div>
-            </div>
+          <section className="space-y-6 ">
+            <LampDemo />
           </section>
         </main>
         <footer className="flex w-full shrink-0 flex-col items-center gap-2 border-t px-4 py-6 sm:flex-row md:px-6">
