778ace07bfd959417636ce9a34fe0aeb4491ae6a
Benjamin Werner <bewerner23@gmail.com>
2024-01-17 16:49:53 -0500

    Made Theme Toggle more simply (just a button)

--- apps/nextjs/src/app/organization/[organizationSlug]/components/theme-toggle.tsx
+++ apps/nextjs/src/app/organization/[organizationSlug]/components/theme-toggle.tsx
@@ -6,13 +6,6 @@ import { useTheme } from "next-themes";
 
 import { Button } from "@acme/ui/components/ui/button";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@acme/ui/components/ui/dropdown-menu";
-
-import {
   Tooltip,
   TooltipContent,
   TooltipTrigger,
@@ -32,28 +25,21 @@ const TooltipProvider = ({ children }: { children: React.ReactNode }) => {
 export function ThemeToggle() {
   const { setTheme, theme } = useTheme();
 
+  const onClick = () => {
+    if (theme === "light") {
+      setTheme("dark");
+    } else if (theme === "dark") {
+      setTheme("light");
+    }
+  };
+
   return (
-    <DropdownMenu>
-      <TooltipProvider>
-        <DropdownMenuTrigger asChild>
-          <Button variant="ghost" size="icon" className="h-8 w-8">
-            <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-            <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-            <span className="sr-only">Toggle theme</span>
-          </Button>
-        </DropdownMenuTrigger>
-      </TooltipProvider>
-      <DropdownMenuContent>
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <TooltipProvider>
+      <Button variant="ghost" size="icon" className="h-8 w-8" onClick={onClick}>
+        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </TooltipProvider>
   );
 }
