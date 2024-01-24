a90d5a934b8da47e8781da545c243dd73283805b
Walshington <nicholaswalsh77@gmail.com>
2024-01-24 15:24:42 -0500

    Clerk webhook initialized

--- packages/api/fastify-crud/src/fastify/routes/webhook.ts
+++ packages/api/fastify-crud/src/fastify/routes/webhook.ts
@@ -1,10 +1,10 @@
 import { FastifyInstance, FastifyRequest, FastifyReply } from "fastify";
 
-export default async function assistant(fastify: FastifyInstance) {
+export default async function webhook(fastify: FastifyInstance) {
   fastify.post(
     "/clerk",
     async function (request: FastifyRequest, reply: FastifyReply) {
-      reply.send("Hello world!");
+      reply.send("Hello from clerk webhook!");
     },
   );
 }
