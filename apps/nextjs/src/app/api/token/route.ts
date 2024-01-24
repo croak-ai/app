f6b36a8789ff98f19991d3810bb4c0a9e556580f
Walshington <nicholaswalsh77@gmail.com>
2024-01-24 14:24:38 -0500

    Added commenting to session token route on Nextjs client

--- apps/nextjs/src/app/api/token/route.ts
+++ apps/nextjs/src/app/api/token/route.ts
@@ -1,9 +1,8 @@
+/* This route was made to grab the session token from the client */
 import { auth } from "@clerk/nextjs";
 
 export async function GET() {
   const { getToken } = auth();
-
   const sessionToken = await getToken();
-  // This is a simple example that responds with a JSON object
   return Response.json({ sessionToken });
 }
