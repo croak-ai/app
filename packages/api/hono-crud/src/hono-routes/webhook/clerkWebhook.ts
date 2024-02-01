297631b41e175fc2ce912befc63c369e0d49db12
Walshington <nicholaswalsh77@gmail.com>
2024-02-01 14:10:43 -0500

    feat(webhook2.0): Comments added

--- packages/api/hono-crud/src/hono-routes/webhook/clerkWebhook.ts
+++ packages/api/hono-crud/src/hono-routes/webhook/clerkWebhook.ts
@@ -1,3 +1,5 @@
+/* ROUTE PREFIX: /webhook */
+
 import {
   OrganizationMembershipWebhookEvent,
   type EmailAddressJSON,
@@ -11,10 +13,9 @@ import { user } from "@packages/db/schema/tenant";
 import { eq } from "@packages/db";
 
 /*
-Verify integrity of webhook using svix
-Connect to org database using orgId in request
-Based on webhook event either create, update, or delete user
-
+This route handles organization membership webhook events from Clerk.
+If an organization member is added, updated, or removed this code will
+reflect the change in the organizations database
 */
 export const clerkWebhook = new Hono<HonoConfig>();
 
@@ -38,8 +39,8 @@ clerkWebhook.post("/organizationMembership", async (c) => {
       updatedAt: event.data.updated_at,
     };
 
-    //Grab orgId
-    const orgId = event.data.organization.id; //"org_2beC7yJZqgXIXisWvhNJFbWie4Q";
+    /* Grab orgId */
+    const orgId = event.data.organization.id;
     const db = createDb({ c, orgId });
 
     switch (event.type) {
@@ -83,70 +84,13 @@ clerkWebhook.post("/organizationMembership", async (c) => {
   }
 });
 
-clerkWebhook.post("/user", async (c) => {
-  try {
-    const event = (await verifyWebhook(
-      c,
-    )) as OrganizationMembershipWebhookEvent;
-
-    const userData = event.data.public_user_data;
-
-    const userPayload = {
-      userId: userData.user_id,
-      role: event.data.role,
-      firstName: userData.first_name,
-      lastName: userData.last_name,
-      email: userData.identifier,
-      imageUrl: userData.image_url,
-      profileImageUrl: userData.profile_image_url,
-      createdAt: event.data.created_at,
-      updatedAt: event.data.updated_at,
-    };
-
-    //Grab orgId
-    const orgId = event.data.organization.id; //"org_2beC7yJZqgXIXisWvhNJFbWie4Q";
-    const db = createDb({ c, orgId });
-
-    switch (event.type) {
-      /* Create user */
-      case "organizationMembership.created":
-        // Check if the user already exists
-        const existingUser = await db.query.user.findFirst({
-          where: eq(user.userId, userPayload.userId),
-        });
-
-        if (existingUser) {
-          return c.text(`user with id ${userPayload.userId} already exists.`);
-        }
-        await db.insert(user).values(userPayload);
-        return c.text(`User with id ${userPayload.userId} created`, 200);
-      /* Update user */
-      /*This is only hit when a users organizaiton role changes*/
-      case "organizationMembership.updated":
-        await db
-          .update(user)
-          .set(userPayload)
-          .where(eq(user.userId, userPayload.userId));
-        return c.text(`User with id ${userPayload.userId} updated`, 200);
-      /* Delete user */
-      case "organizationMembership.deleted":
-        await db.delete(user).where(eq(user.userId, userPayload.userId));
-        return c.text(`User with id ${userPayload.userId} deleted`, 200);
-      default:
-        throw new HTTPException(500, {
-          message: "Invalid event type",
-        });
-    }
-  } catch (err) {
-    if (err instanceof HTTPException) {
-      throw err;
-    } else {
-      throw new HTTPException(500, {
-        message: `Error in Clerk webhook \n ${err}`,
-      });
-    }
-  }
-});
+/* 
+We will eventually need to sync updated user data with each orgs database.
+Not a priority for now but sometihng to think about.
+The only updates that are currently supported are role updates for 
+users in a specific organization.
+*/
+clerkWebhook.post("/user", async (c) => {});
 
 // function getPrimaryEmail(emails: EmailAddressJSON[], primaryEmailId: string) {
 //   const primaryEmail = emails.find(
