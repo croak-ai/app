505899332adbec124da156fb702a743aae06b397
Spartan <bewerner23@gmail.com>
2024-02-13 02:55:35 -0500

    protecting create workspace page

--- apps/tauri-vite/src/routes/_with-auth/_with-org/create-workspace/route.tsx
+++ apps/tauri-vite/src/routes/_with-auth/_with-org/create-workspace/route.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from "@tanstack/react-router";
 import CreateWorkSpaceForm from "./-components/create-workspace";
+import { Protect } from "@clerk/clerk-react";
 
 export const Route = createFileRoute("/_with-auth/_with-org/create-workspace")({
   component: CreateWorkspace,
@@ -7,10 +8,15 @@ export const Route = createFileRoute("/_with-auth/_with-org/create-workspace")({
 
 function CreateWorkspace() {
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center overflow-auto px-4">
-      <div className="max-h-screen w-[50vh] overflow-auto py-12">
-        <CreateWorkSpaceForm />
+    <Protect
+      permission="org:workspace:create"
+      fallback={<div>You Cannot Create a workspace</div>}
+    >
+      <div className="flex min-h-screen flex-col items-center justify-center overflow-auto px-4">
+        <div className="max-h-screen w-[50vh] overflow-auto py-12">
+          <CreateWorkSpaceForm />
+        </div>
       </div>
-    </div>
+    </Protect>
   );
 }
