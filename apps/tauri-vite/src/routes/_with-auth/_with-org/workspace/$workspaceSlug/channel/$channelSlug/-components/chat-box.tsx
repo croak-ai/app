995349da69757a27d4f6099d8dda0ddc00c2fccc
Spartan <bewerner23@gmail.com>
2024-02-13 22:45:08 -0500

    Adding Milkdown to vite

---
+++ apps/tauri-vite/src/routes/_with-auth/_with-org/workspace/$workspaceSlug/channel/$channelSlug/-components/chat-box.tsx
@@ -0,0 +1,91 @@
+"use client";
+
+import { CodemirrorRef } from "@/components/codemirror";
+import type { MilkdownRef } from "@/components/playground-editor";
+import { useCallback, useRef, useState } from "react";
+import { Switch } from "@acme/ui/components/ui/switch";
+import { trpc } from "@/utils/trpc";
+import { PlaygroundMilkdown } from "@/components/playground-editor";
+import { ControlPanel } from "@/components/playground/control-panel";
+
+const isInDevMode = () => {
+  return process.env.NODE_ENV === "development";
+};
+
+export default function ChatBox({
+  workspaceSlug,
+  channelSlug,
+}: {
+  workspaceSlug: string;
+  channelSlug: string;
+}) {
+  /* TODO: This should contain the last message if not sent */
+  const [content] = useState("");
+  const [devModeEnabled, setDevModeEnabled] = useState(false);
+
+  const createMessage = trpc.createMessage.createMessage.useMutation();
+
+  const lockCodemirror = useRef(false);
+  const milkdownRef = useRef<MilkdownRef>(null);
+  const codemirrorRef = useRef<CodemirrorRef>(null);
+
+  const onMilkdownChange = useCallback((markdown: string) => {
+    const lock = lockCodemirror.current;
+    if (lock) return;
+
+    const codemirror = codemirrorRef.current;
+    if (!codemirror) return;
+    codemirror.update(markdown);
+  }, []);
+
+  const onCodemirrorChange = useCallback((getCode: () => string) => {
+    const { current } = milkdownRef;
+    if (!current) return;
+    const value = getCode();
+    current.update(value);
+  }, []);
+
+  const sendMessage = useCallback(async (message: string) => {
+    if (message.trim() === "") return;
+
+    await createMessage.mutate({
+      workspaceSlug: workspaceSlug,
+      channelSlug: channelSlug,
+      messageContent: message,
+    });
+  }, []);
+
+  return (
+    <div className="relative h-screen">
+      <div className="absolute bottom-24 w-full p-4">
+        {devModeEnabled && (
+          <ControlPanel
+            codemirrorRef={codemirrorRef}
+            content={content}
+            onChange={onCodemirrorChange}
+            lock={lockCodemirror}
+          />
+        )}
+
+        <PlaygroundMilkdown
+          milkdownRef={milkdownRef}
+          defaultContent={content}
+          onChange={onMilkdownChange}
+          onSendPressed={(content) => {
+            sendMessage(content);
+          }}
+        />
+        {isInDevMode() && (
+          <div className="flex items-center justify-end">
+            <Switch
+              checked={devModeEnabled}
+              onCheckedChange={setDevModeEnabled}
+              className="mr-2"
+            />
+            <span className="text-sm">Dev Mode</span>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+}
--- apps/tauri-vite/src/routes/_with-auth/_with-org/workspace/$workspaceSlug/channel/$channelSlug/route.tsx
+++ apps/tauri-vite/src/routes/_with-auth/_with-org/workspace/$workspaceSlug/channel/$channelSlug/route.tsx
@@ -1,11 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ChatBoxProvider from "./-components/chat-box-provider";
+import ChatBox from "./-components/chat-box";
 
 export const Route = createFileRoute(
   "/_with-auth/_with-org/workspace/$workspaceSlug/channel/$channelSlug",
 )({
-  component: () => (
-    <div>
-      Hello /_with-auth/_with-org/workspace/$workspaceSlug/channel/$channelSlug!
-    </div>
-  ),
+  component: Channel,
 });
+
+function Channel() {
+  const { workspaceSlug, channelSlug } = Route.useParams();
+  return (
+    <ChatBoxProvider>
+      <ChatBox workspaceSlug={workspaceSlug} channelSlug={channelSlug} />
+    </ChatBoxProvider>
+  );
+}
