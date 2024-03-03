56d11dd069a03159807e917da752f8f856430301
Walshington <nicholaswalsh77@gmail.com>
2024-03-03 18:08:26 -0500

    feat(AIMessageSummaries): Change query to put messages in descending order.

--- packages/api/hono-crud/src/trpc/functions/summarizeMessages.ts
+++ packages/api/hono-crud/src/trpc/functions/summarizeMessages.ts
@@ -41,16 +41,51 @@ export async function summarizeMessages(
       .select({
         userId: message.userId,
         message: message.message,
-        createdAt: message.createdAt,
-        updatedAt: message.updatedAt,
       })
       .from(conversationMessage)
       .innerJoin(message, eq(message.id, conversationMessage.messageId))
-      .where(eq(conversationMessage.conversationId, conversationId));
+      .where(eq(conversationMessage.conversationId, conversationId))
+      .orderBy(desc(message.createdAt));
 
     console.log("WHOLE OBJ: ", conversationMessages);
     console.log("UNSUMMARIZED1: ", conversationMessages[0]);
     console.log("UNSUMMARIZED2: ", conversationMessages[1]);
+
+    const conversationMessagesJSON = JSON.stringify(conversationMessages);
+
+    const completion = await openAI.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: `You are a summarization bot that is tasked with summarizing a 
+          group of messages in a conversation. Summaries should be as succint as the 
+          level of detail provided in the conversation. If specific details are provided
+          in the conversation you should mention them in your summary. However small talk
+          and irrelevant details should be omitted. 
+
+          Use your own discretion on what you think should be kept or ommitted.
+          Only respond with the summary of the conversation.
+          
+          Group of messages: ${conversationMessagesJSON}
+          `,
+        },
+      ],
+      model: "gpt-3.5-turbo",
+    });
+
+    if (!completion.choices[0]?.message.content) {
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Request to OpenAI failed",
+      });
+    }
+
+    /* Parse and validate response */
+    const AIResponse = completion.choices[0].message.content;
+
+    console.log(AIResponse);
+
+    // Generate embedding and add to vector DB
 
     return;
   } catch (error) {
