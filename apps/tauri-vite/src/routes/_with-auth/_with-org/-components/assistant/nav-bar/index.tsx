1a0c2d1f28b23b484e11918001b3c79e1824b847
Walshington <nicholaswalsh77@gmail.com>
2024-03-11 12:59:06 -0400

    feat(thread-history): Add dropdown and search combobox to assistant

--- apps/tauri-vite/src/routes/_with-auth/_with-org/-components/assistant/nav-bar/index.tsx
+++ apps/tauri-vite/src/routes/_with-auth/_with-org/-components/assistant/nav-bar/index.tsx
@@ -2,28 +2,27 @@
 
 import { useState } from "react";
 import { Button } from "@acme/ui/components/ui/button";
-import Dropdown from "./dropdown";
+import NavButton from "./nav-button";
+import ComboBox from "./Combo-Box";
 
 export default function NavBar() {
   const [activeButton, setActiveButton] = useState("CHAT");
+  const [dropdownOpen, setDropdownOpen] = useState(false);
+  //Send activeButton state to navbarButton to refactor in future
 
   return (
     <div className="m-2 w-full">
       <div className="flex w-full items-center justify-between">
-        <div className="flex">
-          <Button
+        <div className="flex flex-grow px-1">
+          {/* <Button
             className={`mx-2 h-6 rounded-none bg-transparent px-0 text-white hover:bg-transparent hover:text-white ${
               activeButton === "CHAT" ? "border-b border-white" : ""
             }`}
             onClick={() => setActiveButton("CHAT")}
           >
             CHAT
-          </Button>
-          <Dropdown
-            buttonName="MORE"
-            activeButton={activeButton}
-            setActiveButton={setActiveButton}
-          />
+          </Button> */}
+          <ComboBox />
         </div>
 
         <Button className="mx-2 h-6 rounded-none bg-transparent px-0 text-white hover:bg-transparent hover:text-white">
