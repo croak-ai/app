22eedc6668071e9f45a2e27b5bd7607735bef136
Spartan <bewerner23@gmail.com>
2024-03-15 11:53:38 -0400

    improving meeting list

--- apps/tauri-vite/src/routes/_with-auth/_with-org/meetings/-components/meeting-list.tsx
+++ apps/tauri-vite/src/routes/_with-auth/_with-org/meetings/-components/meeting-list.tsx
@@ -1,16 +1,16 @@
-import { useState } from "react";
 import { trpc } from "@/utils/trpc";
+import { Link } from "@tanstack/react-router";
+import formatDistanceToNow from "date-fns/formatDistanceToNow";
+import { ScrollArea } from "@acme/ui/components/ui/scroll-area";
+import { Badge } from "@acme/ui/components/ui/badge";
+import { Crown } from "lucide-react";
 
 export default function MeetingList() {
-  const [meetingSheetOpen, setMeetingSheetOpen] = useState(false);
-
-  const utils = trpc.useUtils();
-
   const userMeetings = trpc.getUserMeetings.getUserMeetings.useQuery();
 
-  const Meetings = () => {
+  const MeetingItems = () => {
     if (userMeetings.isLoading || !userMeetings.data) {
-      return <div></div>;
+      return <div>Loading...</div>;
     }
 
     if (userMeetings.data.length === 0) {
@@ -18,25 +18,58 @@ export default function MeetingList() {
     }
 
     return (
-      <div className="flex flex-col">
-        {userMeetings.data.map((data) => (
-          <div key={data.meeting.id} className="border-b border-gray-200 p-4">
-            <h3 className="text-lg font-semibold">{data.meeting.name}</h3>
-            <p className="text-sm text-gray-600">{data.meeting.description}</p>
-            <p className="text-sm text-gray-600">
-              Scheduled:{" "}
-              {new Date(data.meeting.scheduledStart).toLocaleString()} -{" "}
-              {new Date(data.meeting.scheduledEnd).toLocaleString()}
-            </p>
-          </div>
-        ))}
-      </div>
+      <ScrollArea className="h-screen">
+        <div className="flex flex-col gap-2 p-4 pt-0">
+          {userMeetings.data.map((meeting) => (
+            <Link
+              to={`/meetings`}
+              key={meeting.meeting.id}
+              className={`flex flex-col items-start gap-2 rounded-lg border p-3 text-left text-sm transition-all hover:bg-accent ${
+                "asd" === meeting.meeting.id ? "bg-muted" : ""
+              }`}
+            >
+              <div className="flex w-full flex-col gap-1">
+                <div className="flex items-center justify-between">
+                  <div className="font-semibold">{meeting.meeting.name}</div>
+                  <div className="text-xs">
+                    {formatDistanceToNow(
+                      new Date(meeting.meeting.scheduledStart),
+                      {
+                        addSuffix: true,
+                      },
+                    )}
+                  </div>
+                </div>
+                <div className="text-xs font-medium">
+                  {meeting.meeting.description}
+                </div>
+              </div>
+              <div className="line-clamp-2 text-xs text-muted-foreground">
+                Scheduled:{" "}
+                {new Date(meeting.meeting.scheduledStart).toLocaleString()} -{" "}
+                {new Date(meeting.meeting.scheduledEnd).toLocaleString()}
+              </div>
+              <div className="flex items-center gap-2">
+                {meeting.meetingMember.bIsRequiredToAttend === 1 && (
+                  <Badge variant="default">Required</Badge>
+                )}
+                {meeting.meetingMember.bIsHost === 1 && (
+                  <Badge variant="outline">
+                    <Crown className="mr-2 h-4 w-4" />
+                    Host
+                  </Badge>
+                )}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </ScrollArea>
     );
   };
 
   return (
     <div className="flex h-full w-full flex-col ">
-      <Meetings />
+      <MeetingItems />
     </div>
   );
 }
