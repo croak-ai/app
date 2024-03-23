12b51fbf7b7085565c8e5e6841d2c3e4b34b379d
Spartan <bewerner23@gmail.com>
2024-03-23 09:48:22 -0400

    fixing sql migration

--- packages/db/migrations/0000_smooth_terror.sql
+++ packages/db/migrations/0000_smooth_terror.sql
@@ -160,6 +160,26 @@ CREATE TABLE `workspaceMember` (
 	`deletedAt` integer
 );
 --> statement-breakpoint
+
+CREATE VIRTUAL TABLE user_fts USING fts5(firstName, lastName, fullName, email, content="user", content_rowid="internalId");
+--> statement-breakpoint
+
+CREATE TRIGGER user_ai AFTER INSERT ON user BEGIN
+    INSERT INTO user_fts(rowid, firstName, lastName, fullName, email) VALUES (new.internalId, new.firstName, new.lastName, new.fullName, new.email);
+END;    
+--> statement-breakpoint
+
+CREATE TRIGGER user_ad AFTER DELETE ON user BEGIN
+    INSERT INTO user_fts(user_fts, rowid, firstName, lastName, fullName, email) VALUES('delete', old.internalId, old.firstName, old.lastName, old.fullName, old.email);
+END;
+--> statement-breakpoint
+
+CREATE TRIGGER user_au AFTER UPDATE ON user BEGIN
+    INSERT INTO user_fts(user_fts, rowid, firstName, lastName, fullName, email) VALUES('delete', old.internalId, old.firstName, old.lastName, old.fullName, old.email);
+    INSERT INTO user_fts(rowid, firstName, lastName, fullName, email) VALUES (new.internalId, new.firstName, new.lastName, new.fullName, new.email);
+END;
+--> statement-breakpoint
+
 CREATE UNIQUE INDEX `channel_workspaceId_slug_unique` ON `channel` (`workspaceId`,`slug`);--> statement-breakpoint
 CREATE UNIQUE INDEX `meeting_name_unique` ON `meeting` (`name`);--> statement-breakpoint
 CREATE UNIQUE INDEX `recurringMeeting_name_unique` ON `recurringMeeting` (`name`);--> statement-breakpoint
